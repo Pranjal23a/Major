ebf23d642e85315c96fce16626863eb05247e35c
Pranjal Sharma <sharmapranjal23a@gmail.com>
2023-11-23 00:56:07 +0530

    Changes in connecting mongoose

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGO_URL);
-mongoose.connect(`mongodb://127.0.0.1:27017/inventory_data`);
+// mongoose.connect(`mongodb://127.0.0.1:27017/inventory_data`);
 
 const db = mongoose.connection;
 
